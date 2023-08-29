fb62635cab95e6e12d395c64d0a6cc2336472a04
Norman Pendzich <Norman.Pendzich@damago.eu>
2023-08-29 15:41:03 +0200

    lüppt

--- src/components/Control/index.js
+++ src/components/Control/index.js
@@ -2,29 +2,66 @@ import React from 'react';
 
 function Control({handleAction}) {
     return (
-        <div className='Control'>
-            <button onClick={() => handleAction('forward')}>Forward</button>
-            <button onClick={() => handleAction('backward')}>Backward</button>
-            <button onClick={() => handleAction('left')}>Left</button>
-            <button onClick={() => handleAction('right')}>Right</button>
-            <button onClick={() => handleAction('stop')}>Stop</button>
-            <button onClick={() => handleAction('sidestepleft')}>
-                Sidestep Left
-            </button>
-            <button onClick={() => handleAction('sidestepright')}>
-                Sidestep Right
-            </button>
-            <button onClick={() => handleAction('forwardleft')}>
+        <div className='Control grid grid-cols-3 gap-4'>
+            <button
+                onClick={() => handleAction('forwardleft')}
+                className='mr-3'
+            >
                 Forward Left
             </button>
-            <button onClick={() => handleAction('forwardright')}>
+            <button
+                onClick={() => handleAction('forward')}
+                className='mr-3 border-4 '
+            >
+                Forward
+            </button>
+            <button
+                onClick={() => handleAction('forwardright')}
+                className='mr-3'
+            >
                 Forward Right
             </button>
-            <button onClick={() => handleAction('backwardleft')}>
+
+            <button onClick={() => handleAction('left')} className='mr-3'>
+                Left
+            </button>
+
+            <button onClick={() => handleAction('stop')} className='mr-3'>
+                Stop
+            </button>
+            <button onClick={() => handleAction('right')} className='mr-3'>
+                Right
+            </button>
+
+            <button
+                onClick={() => handleAction('backwardleft')}
+                className='mr-3'
+            >
                 Backward Left
             </button>
-            <button onClick={() => handleAction('backwardright')}>
-                Forward Right
+
+            <button onClick={() => handleAction('backward')} className='mr-3'>
+                Backward
+            </button>
+
+            <button
+                onClick={() => handleAction('backwardright')}
+                className='mr-3'
+            >
+                Backward Right
+            </button>
+            <button
+                onClick={() => handleAction('sidestepright')}
+                className='mr-3'
+            >
+                Sidestep Right
+            </button>
+            <button className='mr-3'>Platzhalter</button>
+            <button
+                onClick={() => handleAction('sidestepleft')}
+                className='mr-3'
+            >
+                Sidestep Left
             </button>
         </div>
     );
