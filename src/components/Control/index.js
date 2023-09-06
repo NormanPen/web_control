ca84b32261eae2f88920cc42af244559bed1c392
Norman Pendzich <73986750+NormanPen@users.noreply.github.com>
2023-09-06 19:20:39 +0200

    sieht schon besser aus!

--- src/components/Control/index.js
+++ src/components/Control/index.js
@@ -1,8 +1,17 @@
 import React, {useState} from 'react';
-import pfeil from '../../images/arrow.png';
+import './style.css';
+
 import stop from '../../images/stop.png';
-import play from '../../images/play.png';
-import double from '../../images/double.png';
+import links from '../../images/links.png';
+import oben from '../../images/oben.png';
+import rechts from '../../images/rechts.png';
+import unten from '../../images/unten.png';
+import obenlinks from '../../images/obenlinks.png';
+import obenrechts from '../../images/obenrechts.png';
+import untenlinks from '../../images/untenlinks.png';
+import untenrechts from '../../images/untenrechts.png';
+import linkslinks from '../../images/linkslinks.png';
+import rechtsrechts from '../../images/rechtsrechts.png';
 
 function Control({handleAction}) {
     const [isAutoMode, setIsAutoMode] = useState(false);
@@ -24,65 +33,67 @@ function Control({handleAction}) {
     };
 
     return (
-        <div className='Control grid grid-cols-3 gap-4 mt-9'>
+        <div className='Control grid grid-cols-3 gap-4 mt-9 justify-items-stretch'>
             <button
                 onClick={() => handleAction('forwardleft')}
-                className='mr-3'
+                className='justify-self-center'
             >
-                <img src={pfeil} alt='pfeil' className=' rotate-180' />
+                <img src={obenlinks} alt='pfeil' className=' ' />
             </button>
-            <button onClick={() => handleAction('forward')} className='mr-3 '>
-                <img src={pfeil} alt='pfeil' className=' rotate-[270deg] ' />
+
+            <button onClick={() => handleAction('forward')} className='justify-self-center '>
+                <img src={oben} alt='pfeil' className='  ' />
             </button>
+
             <button
                 onClick={() => handleAction('forwardright')}
-                className='mr-3'
+                className='justify-self-center'
             >
-                <img src={pfeil} alt='pfeil' className='  ' />
+                <img src={obenrechts} alt='obenrechts' className='  ' />
             </button>
 
-            <button onClick={() => handleAction('left')} className='mr-3'>
-                <img src={pfeil} alt='pfeil' className=' rotate-180' />
+            <button onClick={() => handleAction('left')} className='justify-self-center'>
+                <img src={links} alt='pfeil' className=' ' />
             </button>
 
-            <button onClick={() => handleAction('stop')} className='mr-3'>
-                <img src={stop} alt='stop' className=' rotate-180' />
+            <button onClick={() => handleAction('stop')} className='justify-self-center'>
+                <img src={stop} alt='stop' className=' ' />
             </button>
-            <button onClick={() => handleAction('right')} className='mr-3'>
-                <img src={pfeil} alt='pfeil' className='  ' />
+            <button onClick={() => handleAction('right')} className='justify-self-center'>
+                <img src={rechts} alt='pfeil' className='  ' />
             </button>
 
             <button
                 onClick={() => handleAction('backwardleft')}
-                className='mr-3'
+                className='justify-self-center'
             >
-                <img src={pfeil} alt='pfeil' className=' rotate-180' />
+                <img src={untenlinks} alt='pfeil' className=' ' />
             </button>
 
-            <button onClick={() => handleAction('backward')} className='mr-3'>
-                <img src={pfeil} alt='pfeil' className=' rotate-90' />
+            <button onClick={() => handleAction('backward')} className='justify-self-center'>
+                <img src={unten} alt='pfeil' className=' ' />
             </button>
 
             <button
                 onClick={() => handleAction('backwardright')}
-                className='mr-3'
+                className='justify-self-center'
             >
-                <img src={pfeil} alt='pfeil' className='  ' />
+                <img src={untenrechts} alt='pfeil' className='  ' />
             </button>
             <button
                 onClick={() => handleAction('sidestepleft')}
-                className='mr-3'
+                className='justify-self-center'
             >
-                <img src={double} alt='pfeil' className=' rotate-180 ' />
+                <img src={linkslinks} alt='linkslinks' className='  ' />
             </button>
-            <button onClick={toggleAutoMode} className='mr-3'>
+            <button onClick={toggleAutoMode} className='auto justify-self-center'>
                 {isAutoMode ? 'Stop Auto mode' : 'Start Auto mode'}
             </button>
             <button
                 onClick={() => handleAction('sidestepright')}
-                className='mr-3'
+                className='justify-self-center'
             >
-                <img src={double} alt='pfeil' className='  ' />
+                <img src={rechtsrechts} alt='rechtsrechts' className='  ' />
             </button>
         </div>
     );
